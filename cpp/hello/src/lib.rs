1165caf85b26efd8a47ce10fc260368c593da57c
krazijames <krazijames@gmail.com>
2021-02-02 00:16:42 +0900

    Bind C++

--- cpp/hello/src/lib.rs
+++ cpp/hello/src/lib.rs
@@ -1,4 +1,16 @@
+use std::os::raw::c_char;
+use std::ffi::CStr;
+use std::ffi::CString;
+
 #[no_mangle]
-pub extern fn greet() {
-    hello::greet();
+pub extern fn print_string(name: *const c_char) {
+    let c_str: &CStr = unsafe { CStr::from_ptr(name) };
+    hello::print_string(c_str.to_str().unwrap());
+}
+
+#[no_mangle]
+pub extern fn get_string(name: *const c_char) -> *mut c_char {
+    let c_name: &CStr = unsafe { CStr::from_ptr(name) };
+    let string = hello::get_string(c_name.to_str().unwrap());
+    CString::new(string).unwrap().into_raw()
 }
--- cpp/hello/tests/test_hello.cpp
+++ cpp/hello/tests/test_hello.cpp
@@ -1,7 +1,19 @@
 #include <hello.h>
+#include <cstring>
+#include <cstdio>
 
 int test_hello(int argc, char* argv[]) {
-  hello::greet();
+  hello::print_string("world");
+
+  char* string = hello::get_string("world");
+
+  int result = strcmp(string, "Hello world!");
+
+  free(string);
+
+  if (result) {
+    return -1;
+  }
 
   return 0;
 }
