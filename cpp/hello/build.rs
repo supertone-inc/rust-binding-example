f9299118fcc642a46eefb60937663140d679ffc1
krazijames <krazijames@gmail.com>
2021-02-03 13:28:49 +0900

    Move `cbindgen` task into `build.rs`

---
+++ cpp/hello/build.rs
@@ -0,0 +1,15 @@
+extern crate cbindgen;
+
+use std::env;
+
+fn main() {
+  let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+  let project_name = env::var("PROJECT_NAME").unwrap();
+  let include_dir = format!("{}/include", project_name);
+  let header_file_name = format!("{}.h", project_name);
+  let header_file_path = format!("{}/{}", include_dir, header_file_name);
+
+  cbindgen::generate(crate_dir)
+    .unwrap()
+    .write_to_file(header_file_path);
+}
