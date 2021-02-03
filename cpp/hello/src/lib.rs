bd7bedfa4f2863f3fa47ad6882270e118d207b62
krazijames <krazijames@gmail.com>
2021-02-03 17:45:16 +0900

    Add safe examples

--- cpp/hello/src/lib.rs
+++ cpp/hello/src/lib.rs
@@ -2,15 +2,25 @@ use std::ffi::{CStr, CString};
 use libc::{c_char, c_float, size_t};
 
 #[no_mangle]
-pub extern fn to_uppercase(s: *const c_char) -> *mut c_char {
-    let s = unsafe { CStr::from_ptr(s) }.to_str().unwrap();
-    let string = hello::to_uppercase(s);
+pub extern fn to_uppercase(string: *const c_char) -> *mut c_char {
+    let string = unsafe { CStr::from_ptr(string) }.to_str().unwrap();
+    let string = hello::to_uppercase(string);
     CString::new(string).unwrap().into_raw()
 }
 
 #[no_mangle]
 pub unsafe extern fn destroy_string(string: *mut c_char) {
     let _ = CString::from_raw(string);
+}
+
+#[no_mangle]
+pub extern fn to_uppercase_safe(in_string: *const c_char, out_string: *mut c_char) {
+    let in_string = unsafe { CStr::from_ptr(in_string) }.to_str().unwrap();
+    let string = hello::to_uppercase(in_string);
+    let string = CString::new(string).unwrap();
+    let bytes = string.as_bytes();
+
+    unsafe { std::ptr::copy_nonoverlapping(bytes.as_ptr(), out_string as *mut u8, bytes.len()) }
 }
 
 #[no_mangle]
@@ -24,4 +34,13 @@ pub extern fn concat(a: *const c_float, a_length: size_t, b: *const c_float, b_l
 #[no_mangle]
 pub unsafe extern fn destroy_array(array: *mut c_float) {
     let _ = Box::from_raw(array);
+}
+
+#[no_mangle]
+pub extern fn concat_safe(a: *const c_float, a_length: size_t, b: *const c_float, b_length: size_t, out_array: *mut c_float) {
+    let a: &[f32] = unsafe { std::slice::from_raw_parts(a, a_length) };
+    let b: &[f32] = unsafe { std::slice::from_raw_parts(b, b_length) };
+    let c = hello::concat(a, b);
+
+    unsafe { std::ptr::copy_nonoverlapping(c.as_ptr(), out_array, c.len()) }
 }
--- cpp/hello/tests/main.cpp
+++ cpp/hello/tests/main.cpp
@@ -3,7 +3,7 @@
 
 #include <hello.h>
 
-TEST_CASE("test_to_uppercase", "[hello]") {
+TEST_CASE("test_to_uppercase") {
   char* string = hello::to_uppercase("Hello World!");
 
   CHECK(std::string(string) == "HELLO WORLD!");
@@ -11,10 +11,24 @@ TEST_CASE("test_to_uppercase", "[hello]") {
   hello::destroy_string(string);
 }
 
-TEST_CASE("test_concat", "[hello]") {
+TEST_CASE("test_to_uppercase_safe") {
+  std::string in_string("Hello World!");
+  std::string out_string = in_string;
+
+  hello::to_uppercase_safe(in_string.data(), (char *)out_string.data());
+
+  CHECK(in_string == "Hello World!");
+  CHECK(out_string == "HELLO WORLD!");
+}
+
+TEST_CASE("test_concat") {
   float a[] = {1.f, 2.f};
+  size_t a_length = sizeof(a) / sizeof(a[0]);
+
   float b[] = {3.f, 4.f, 5.f};
-  float* array = hello::concat(a, 2, b, 3);
+  size_t b_length = sizeof(b) / sizeof(b[0]);
+
+  float* array = hello::concat(a, a_length, b, b_length);
 
   CHECK(array[0] == 1.f);
   CHECK(array[1] == 2.f);
@@ -23,4 +37,22 @@ TEST_CASE("test_concat", "[hello]") {
   CHECK(array[4] == 5.f);
 
   hello::destroy_array(array);
+}
+
+TEST_CASE("test_concat_safe") {
+  float a[] = {1.f, 2.f};
+  size_t a_length = sizeof(a) / sizeof(a[0]);
+
+  float b[] = {3.f, 4.f, 5.f};
+  size_t b_length = sizeof(b) / sizeof(b[0]);
+
+  float c[a_length + b_length];
+
+  hello::concat_safe(a, a_length, b, b_length, c);
+
+  CHECK(c[0] == 1.f);
+  CHECK(c[1] == 2.f);
+  CHECK(c[2] == 3.f);
+  CHECK(c[3] == 4.f);
+  CHECK(c[4] == 5.f);
 }
