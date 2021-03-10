9dd2000e54f011a2d367ff3c9d2775f0e4af214b
krazijames <krazijames@gmail.com>
2021-03-10 13:18:05 +0900

    Bind `concat()`

--- nodejs/hello/__tests__/test_hello.js
+++ nodejs/hello/__tests__/test_hello.js
@@ -3,3 +3,7 @@ const hello = require('..')
 test('to_uppercase', () => {
   expect(hello.to_uppercase('Hello World!')).toBe('HELLO WORLD!')
 })
+
+test('concat', () => {
+  expect(hello.concat([1, 2], [3, 4, 5])).toEqual([1, 2, 3, 4, 5])
+})
--- nodejs/hello/src/lib.rs
+++ nodejs/hello/src/lib.rs
@@ -1,11 +1,14 @@
 #[macro_use]
 extern crate napi_derive;
 
-use napi::{CallContext, JsObject, Result, JsString};
+use std::convert::TryInto;
+
+use napi::{CallContext, JsNumber, JsObject, JsString, Result};
 
 #[module_exports]
 fn init(mut exports: JsObject) -> Result<()> {
   exports.create_named_method("to_uppercase", to_uppercase)?;
+  exports.create_named_method("concat", concat)?;
 
   Ok(())
 }
@@ -15,4 +18,33 @@ fn to_uppercase(ctx: CallContext) -> Result<JsString> {
   let s = ctx.get::<JsString>(0)?.into_utf8()?;
 
   ctx.env.create_string(&hello::to_uppercase(s.as_str()?))
+}
+
+#[js_function(2)]
+fn concat(ctx: CallContext) -> Result<JsObject> {
+  let a_obj: JsObject = ctx.get(0)?;
+  let a_len: u32 = a_obj.get_array_length()?;
+  let a = (0..a_len)
+    .map(|i| {
+      let value: f64 = a_obj.get_element::<JsNumber>(i)?.try_into()?;
+      Ok(value as f32)
+    })
+    .collect::<Result<Vec<f32>>>()?;
+
+  let b_obj: JsObject = ctx.get(1)?;
+  let b_len: u32 = b_obj.get_array_length()?;
+  let b = (0..b_len)
+    .map(|i| {
+      let value: f64 = b_obj.get_element::<JsNumber>(i)?.try_into()?;
+      Ok(value as f32)
+    })
+    .collect::<Result<Vec<f32>>>()?;
+
+  let result: Vec<f32> = hello::concat(&a, &b);
+  let mut array = ctx.env.create_array()?;
+  result.iter().enumerate()
+    .map(|(i, v)| array.set_element(i as u32, ctx.env.create_double(*v as f64)?))
+    .collect::<Result<Vec<()>>>()?;
+
+  Ok(array)
 }
