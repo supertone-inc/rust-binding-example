802238ff923482b88e07fd117a0ce301a19eeeaa
krazijames <krazijames@gmail.com>
2021-07-27 09:11:43 +0900

    Bind `array` module

--- nodejs/hello-neon/src/lib.rs
+++ nodejs/hello-neon/src/lib.rs
@@ -1,9 +1,13 @@
+mod array;
 mod string;
 
 use neon::prelude::*;
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    let array = array::init_module(&mut cx)?;
+    cx.export_value("array", array)?;
+
     let string = string::init_module(&mut cx)?;
     cx.export_value("string", string)?;
 
