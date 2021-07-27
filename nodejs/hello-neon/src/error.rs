1b2e05cf9f2e8469d27f6cd9cd7830547b28a6ea
krazijames <krazijames@gmail.com>
2021-07-27 10:54:54 +0900

    Bind `error` module

---
+++ nodejs/hello-neon/src/error.rs
@@ -0,0 +1,29 @@
+use neon::prelude::*;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    Hello(#[from] hello::Error),
+}
+
+impl Error {
+    pub fn throw<'a, C: Context<'a>>(&self, c: &mut C) -> neon::result::Throw {
+        c.throw_error::<String, JsError>(self.to_string()).err().unwrap()
+    }
+}
+
+pub fn init_module<'a, C: Context<'a>>(c: &mut C) -> JsResult<'a, JsObject> {
+    let exports = JsObject::new(c);
+
+    let raise_error = JsFunction::new(c, raise_error)?;
+    exports.set(c, "raise_error", raise_error)?;
+
+    Ok(exports)
+}
+
+fn raise_error(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    hello::error::raise_error().map_err(|err| Error::from(err).throw(&mut cx))?;
+
+    Ok(cx.undefined())
+}
--- nodejs/hello-neon/src/lib.rs
+++ nodejs/hello-neon/src/lib.rs
@@ -1,4 +1,5 @@
 mod array;
+mod error;
 mod string;
 
 use neon::prelude::*;
@@ -7,6 +8,9 @@ use neon::prelude::*;
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     let array = array::init_module(&mut cx)?;
     cx.export_value("array", array)?;
+
+    let error = error::init_module(&mut cx)?;
+    cx.export_value("error", error)?;
 
     let string = string::init_module(&mut cx)?;
     cx.export_value("string", string)?;
