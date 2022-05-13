a6d829db0669a0e9d4c3c18f207bf3b893ff33f6
krazijames <krazijames@gmail.com>
2022-05-13 10:41:44 +0900

    Rename

--- wasm/hello/init-page.js
+++ wasm/hello/init-page.js
@@ -1,9 +1,7 @@
 module.exports = async () => {
   await page.goto("http://localhost:3000");
   await page.evaluate(async () => {
-    const { default: init, ...hello } = await import(
-      "/hello-wasm/hello_wasm.js"
-    );
+    const { default: init, ...hello } = await import("/hello/index.js");
     await init();
     window.hello = hello;
   });
