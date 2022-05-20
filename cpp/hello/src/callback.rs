979a28e9d89a09e4493eac314dc085e1a664dd06
krazijames <krazijames@gmail.com>
2022-05-20 17:53:07 +0900

    Rename

--- cpp/hello/src/callback.rs
+++ cpp/hello/src/callback.rs
@@ -2,13 +2,13 @@ use libc::{c_float, c_int, size_t};
 
 #[no_mangle]
 pub extern "C" fn hello__callback__map(
-    items_in: *const c_int,
-    items_out: *mut c_float,
+    in_items: *const c_int,
+    out_items: *mut c_float,
     item_count: size_t,
     mapper: extern "C" fn(c_int) -> c_float,
 ) {
-    let items: &[c_int] = unsafe { std::slice::from_raw_parts(items_in, item_count) };
+    let items: &[c_int] = unsafe { std::slice::from_raw_parts(in_items, item_count) };
     let mapped_items = hello::callback::map(items, |item| mapper(*item));
 
-    unsafe { std::ptr::copy_nonoverlapping(mapped_items.as_ptr(), items_out, mapped_items.len()) }
+    unsafe { std::ptr::copy_nonoverlapping(mapped_items.as_ptr(), out_items, item_count) }
 }
--- cpp/hello/tests/test_callback.cpp
+++ cpp/hello/tests/test_callback.cpp
@@ -9,16 +9,16 @@ float mapper(int item)
 
 TEST_CASE("[C] hello__callback__map()")
 {
-    int items_in[] = {1, 2, 3};
-    const size_t item_count = sizeof(items_in) / sizeof(items_in[0]);
+    int in_items[] = {1, 2, 3};
+    const size_t item_count = sizeof(in_items) / sizeof(in_items[0]);
 
-    float items_out[item_count];
+    float out_items[item_count];
 
-    hello__callback__map(items_in, items_out, item_count, mapper);
+    hello__callback__map(in_items, out_items, item_count, mapper);
 
-    REQUIRE(items_out[0] == 0.5f);
-    REQUIRE(items_out[1] == 1.0f);
-    REQUIRE(items_out[2] == 1.5f);
+    REQUIRE(out_items[0] == 0.5f);
+    REQUIRE(out_items[1] == 1.0f);
+    REQUIRE(out_items[2] == 1.5f);
 }
 
 TEST_CASE("[C++] hello::callback::map()")
