4f8d16d8510d45cc57dd96892b736d7887bbde91
krazijames <krazijames@gmail.com>
2022-05-20 17:09:55 +0900

    Remove unnecessary type conversion

--- wasm/hello/__tests__/test_array.js
+++ wasm/hello/__tests__/test_array.js
@@ -3,6 +3,15 @@ const initPage = require("../init-page");
 beforeAll(initPage);
 
 test("concat", async () => {
-  const result = await page.evaluate(() => hello.concat([1, 2], [3, 4, 5]));
-  expect(result).toEqual([1, 2, 3, 4, 5]);
+  const serdeResult = await page.evaluate(() => {
+    const result = hello.concat([1, 2], [3, 4, 5]);
+
+    if (!(result instanceof Float32Array)) {
+      throw "incorrect result type";
+    }
+
+    return result;
+  });
+
+  expect(Object.values(serdeResult)).toEqual([1, 2, 3, 4, 5]);
 });
--- wasm/hello/src/array.rs
+++ wasm/hello/src/array.rs
@@ -1,11 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn concat(a: Vec<JsValue>, b: Vec<JsValue>) -> Vec<JsValue> {
-    let a: Vec<f32> = a.iter().map(|v| v.unchecked_into_f64() as f32).collect();
-    let b: Vec<f32> = b.iter().map(|v| v.unchecked_into_f64() as f32).collect();
+pub fn concat(a: &[f32], b: &[f32]) -> Vec<f32> {
     hello::array::concat(&a, &b)
-        .into_iter()
-        .map(|v| JsValue::from(v))
-        .collect()
 }
