7c537acffedddd8d4f97e791527f288433a67b88
krazijames <krazijames@gmail.com>
2022-05-20 15:47:20 +0900

    Utilize `into_iter()`

--- nodejs/hello/src/array.rs
+++ nodejs/hello/src/array.rs
@@ -2,11 +2,11 @@
 pub mod array {
     #[napi]
     pub fn concat(a: Vec<f64>, b: Vec<f64>) -> Vec<f64> {
-        let a: Vec<f32> = a.iter().map(|v| *v as f32).collect();
-        let b: Vec<f32> = b.iter().map(|v| *v as f32).collect();
+        let a: Vec<f32> = a.into_iter().map(|v| v as f32).collect();
+        let b: Vec<f32> = b.into_iter().map(|v| v as f32).collect();
         hello::array::concat(&a, &b)
-            .iter()
-            .map(|v| *v as f64)
+            .into_iter()
+            .map(|v| v as f64)
             .collect()
     }
 }
--- wasm/hello/src/array.rs
+++ wasm/hello/src/array.rs
@@ -5,7 +5,7 @@ pub fn concat(a: Box<[JsValue]>, b: Box<[JsValue]>) -> Box<[JsValue]> {
     let a: Vec<f32> = a.iter().map(|v| v.unchecked_into_f64() as f32).collect();
     let b: Vec<f32> = b.iter().map(|v| v.unchecked_into_f64() as f32).collect();
     hello::array::concat(&a, &b)
-        .iter()
-        .map(|v| JsValue::from(*v))
+        .into_iter()
+        .map(|v| JsValue::from(v))
         .collect()
 }
