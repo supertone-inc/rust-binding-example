44dbbb080d3946a479c11fca0a76de55e0d948ea
krazijames <krazijames@gmail.com>
2022-05-20 16:41:40 +0900

    Remove unnecessary boxing

--- wasm/hello/src/array.rs
+++ wasm/hello/src/array.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn concat(a: Box<[JsValue]>, b: Box<[JsValue]>) -> Box<[JsValue]> {
+pub fn concat(a: Vec<JsValue>, b: Vec<JsValue>) -> Vec<JsValue> {
     let a: Vec<f32> = a.iter().map(|v| v.unchecked_into_f64() as f32).collect();
     let b: Vec<f32> = b.iter().map(|v| v.unchecked_into_f64() as f32).collect();
     hello::array::concat(&a, &b)
--- wasm/hello/src/callback.rs
+++ wasm/hello/src/callback.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn map(items: Box<[JsValue]>, mapper: &js_sys::Function) -> Result<Vec<JsValue>, JsValue> {
+pub fn map(items: Vec<JsValue>, mapper: &js_sys::Function) -> Result<Vec<JsValue>, JsValue> {
     let this = JsValue::null();
     hello::callback::map(&items, |item| mapper.call1(&this, item))
         .into_iter()
