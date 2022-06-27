b40c7c403182c788068f6535af8eb1a35d4fca3e
whybe <whybe@supertone.ai>
2022-06-28 01:25:16 +0900

    more jest-puppeteer config

--- wasm/hello-emscripten/tests/jest-puppeteer.config.js
+++ wasm/hello-emscripten/tests/jest-puppeteer.config.js
@@ -3,4 +3,12 @@ module.exports = {
     command: "npx serve ..",
     port: 3000,
   },
+  launch: {
+    headless: true,
+    // product: "chrome",
+    // args: ["--start-maximized"],
+    // defaultViewport: { width: 1700, height: 800 },
+    // slowMo: 250,
+    // devtools: true,
+  },
 };
