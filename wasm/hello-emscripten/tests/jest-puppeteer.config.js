7a739c993674dad4eba4240def3d71323e7b1de3
krazijames <krazijames@gmail.com>
2022-06-29 14:32:51 +0900

    Revert "more jest-puppeteer config"
    This reverts commit b40c7c403182c788068f6535af8eb1a35d4fca3e.

--- wasm/hello-emscripten/tests/jest-puppeteer.config.js
+++ wasm/hello-emscripten/tests/jest-puppeteer.config.js
@@ -3,12 +3,4 @@ module.exports = {
     command: "npx serve ..",
     port: 3000,
   },
-  launch: {
-    headless: true,
-    // product: "chrome",
-    // args: ["--start-maximized"],
-    // defaultViewport: { width: 1700, height: 800 },
-    // slowMo: 250,
-    // devtools: true,
-  },
 };
