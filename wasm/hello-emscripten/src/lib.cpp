be0e061d91588e13ba1624ccf53edc8a9a67d5dd
krazijames <krazijames@gmail.com>
2022-06-29 17:40:53 +0900

    Implement `concatPrealloc()`

--- wasm/hello-emscripten/src/lib.cpp
+++ wasm/hello-emscripten/src/lib.cpp
@@ -4,21 +4,38 @@
 #include <hello_string.hpp>
 
 #include <iostream>
+#include <vector>
 
 namespace em = emscripten;
+
+template <typename T> em::val vector_to_typed_array(const std::vector<T> &vec)
+{
+    return em::val{em::typed_memory_view(vec.size(), vec.data())};
+}
 
 em::val concat(const em::val &a, const em::val &b)
 {
     auto a_vec{em::convertJSArrayToNumberVector<float>(a)};
     auto b_vec{em::convertJSArrayToNumberVector<float>(b)};
     auto c_vec{hello::array::concat(a_vec, b_vec)};
-    auto c_float32_array{em::typed_memory_view(c_vec.size(), c_vec.data())};
 
-    return em::val::global("Array").call<em::val>("from", c_float32_array);
+    return vector_to_typed_array(c_vec);
+}
+
+void concat_prealloc(const std::vector<float> &a, const std::vector<float> &b, std::vector<float> &c)
+{
+    hello__array__concat(a.data(), a.size(), b.data(), b.size(), c.data());
 }
 
 EMSCRIPTEN_BINDINGS()
 {
-    em::function("to_uppercase", &hello::string::to_uppercase);
+    em::register_vector<float>("Float32Vector")
+        .constructor<size_t>()
+        .class_function("from", &em::convertJSArrayToNumberVector<float>)
+        .function("toTypedArray", &vector_to_typed_array<float>);
+
     em::function("concat", &concat);
+    em::function("concatPrealloc", &concat_prealloc);
+
+    em::function("to_uppercase", &hello::string::to_uppercase);
 }
--- wasm/hello-emscripten/tests/__tests__/test_array.js
+++ wasm/hello-emscripten/tests/__tests__/test_array.js
@@ -5,7 +5,33 @@ beforeAll(initPage);
 test("concat", async () => {
   await expect(
     page.evaluate(() => {
-      return hello.concat([1, 2], [3, 4, 5]);
+      return Array.from(hello.concat([1, 2], [3, 4, 5]));
     })
   ).resolves.toEqual([1, 2, 3, 4, 5]);
+});
+
+test("concatPrealloc", async () => {
+  await expect(
+    page.evaluate(() => {
+      const a = hello.Float32Vector.from([1, 2]);
+      const b = hello.Float32Vector.from([3, 4, 5]);
+      const c = new hello.Float32Vector(5);
+
+      hello.concatPrealloc(a, b, c);
+
+      return Array.from(c.toTypedArray());
+    })
+  ).resolves.toEqual([1, 2, 3, 4, 5]);
+});
+
+test("Float32Vector", async () => {
+  await expect(
+    page.evaluate(() => {
+      const vector = new hello.Float32Vector(3);
+
+      vector.toTypedArray().set([1, 2, 3]);
+
+      return Array.from(vector.toTypedArray());
+    })
+  ).resolves.toEqual([1, 2, 3]);
 });
