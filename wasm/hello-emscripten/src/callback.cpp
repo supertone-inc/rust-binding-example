9c67d834c8d547532f071a922c4d1f539d87d6ea
krazijames <krazijames@gmail.com>
2022-07-10 21:57:11 +0900

    Receive captureing lambda from C++ API

--- wasm/hello-emscripten/src/callback.cpp
+++ wasm/hello-emscripten/src/callback.cpp
@@ -4,15 +4,10 @@
 
 namespace em = emscripten;
 
-em::val *global_mapper = nullptr;
-
 em::val map(const em::val &items, const em::val &mapper)
 {
     auto items_vec{em::convertJSArrayToNumberVector<int>(items)};
-
-    global_mapper = (em::val *)&mapper;
-    auto mapped_items = hello::callback::map(items_vec, [](int item) { return ((*global_mapper)(item)).as<float>(); });
-    global_mapper = nullptr;
+    auto mapped_items = hello::callback::map(items_vec, [&](int item) { return mapper(item).as<float>(); });
 
     return em::val{em::typed_memory_view(mapped_items.size(), mapped_items.data())};
 }
