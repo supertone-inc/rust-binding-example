4d0df9002a8e841dd4932e00bba1b6d2a247c0de
krazijames <krazijames@gmail.com>
2022-07-19 19:06:56 +0900

    Make `structure` example more simple

--- rust/hello/src/structure.rs
+++ rust/hello/src/structure.rs
@@ -1,24 +1,15 @@
-pub struct Greeter {
-    name: String,
+pub struct Counter {
+    count: i32,
 }
 
-impl Greeter {
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: name.to_string(),
-        }
+impl Counter {
+    pub fn new(count: i32) -> Self {
+        Self { count }
     }
 
-    pub fn name(&self) -> String {
-        self.name.clone()
-    }
-
-    pub fn set_name(&mut self, name: &str) {
-        self.name = name.to_string()
-    }
-
-    pub fn greet(&self, subject_name: &str) -> String {
-        format!("Hello {}! I'm {}.", subject_name, self.name)
+    pub fn increase(&mut self, amount: i32) -> i32 {
+        self.count += amount;
+        self.count
     }
 }
 
@@ -27,17 +18,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_name() {
-        let mut greeter = Greeter::new("greeter");
-        assert_eq!(greeter.name(), "greeter");
-
-        greeter.set_name("greater");
-        assert_eq!(greeter.name(), "greater");
-    }
-
-    #[test]
-    fn test_greet() {
-        let greeter = Greeter::new("greeter");
-        assert_eq!(greeter.greet("greetee"), "Hello greetee! I'm greeter.");
+    fn test_counter() {
+        let mut counter = Counter::new(1);
+        assert_eq!(counter.increase(2), 3);
     }
 }
