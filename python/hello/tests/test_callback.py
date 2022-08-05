5c5d78d2715af99b91f9a69a0b19729a73a0eeef
krazijames <krazijames@gmail.com>
2022-08-05 21:53:01 +0900

    Use lambda function for Python

--- python/hello/tests/test_callback.py
+++ python/hello/tests/test_callback.py
@@ -1,7 +1,4 @@
 import hello
 
 def test_map():
-    def mapper(item):
-        return item * 2
-
-    assert hello.callback.map([1, 2, 3], mapper) == [2, 4, 6]
+    assert hello.callback.map([1, 2, 3], lambda item: item * 2) == [2, 4, 6]
