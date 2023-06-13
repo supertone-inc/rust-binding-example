c0c814418435dd21d976f72a2560217209070ef4
krazijames <krazijames@gmail.com>
2023-06-13 17:25:27 +0900

    Update `emsdk.cmake`

--- wasm/hello-emscripten/emsdk.cmake
+++ wasm/hello-emscripten/emsdk.cmake
@@ -1,27 +1,22 @@
-include(FetchContent)
+set(EMSDK_VERSION 3.1.3)
 
+include(FetchContent)
 FetchContent_Declare(
     emsdk
-    GIT_REPOSITORY  https://github.com/emscripten-core/emsdk.git
-    GIT_TAG         3.1.3
-    GIT_SHALLOW     TRUE
-    GIT_PROGRESS    TRUE
+    GIT_REPOSITORY https://github.com/emscripten-core/emsdk.git
+    GIT_TAG ${EMSDK_VERSION}
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
 )
 FetchContent_GetProperties(emsdk)
+
 if(NOT emsdk_POPULATED)
     FetchContent_Populate(emsdk)
 endif()
 
-set(EMSDK_VERSION 3.1.3)
-set(EMSDK_COMMAND ${emsdk_SOURCE_DIR}/emsdk)
+set(EMSDK_DIR ${emsdk_SOURCE_DIR})
 
-execute_process(
-    COMMAND ${EMSDK_COMMAND} install ${EMSDK_VERSION}
-    WORKING_DIRECTORY ${emsdk_SOURCE_DIR}
-)
-execute_process(
-    COMMAND ${EMSDK_COMMAND} activate ${EMSDK_VERSION}
-    WORKING_DIRECTORY ${emsdk_SOURCE_DIR}
-)
+execute_process(COMMAND ${EMSDK_DIR}/emsdk install ${EMSDK_VERSION})
+execute_process(COMMAND ${EMSDK_DIR}/emsdk activate ${EMSDK_VERSION})
 
-set(CMAKE_TOOLCHAIN_FILE "${emsdk_SOURCE_DIR}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" CACHE FILEPATH "emscripten cmake toolchain" FORCE)
+set(CMAKE_TOOLCHAIN_FILE ${EMSDK_DIR}/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake)
